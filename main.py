5cd24816e9357bdbbe7c54489293ff80c2199dda
Ron H <hachmonron@gmail.com>
2023-03-09 00:12:42 +0200

    reafactor

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from RoadDetect import RoadDetect
 
 
-
-RoadDetect().detect('C:\\Users\\97254\\Desktop\\Computer Vision\\project_env\\lane detection\\koko.mp4')
+# insert video path
+RoadDetect().detect('video-path')
 
 
          
